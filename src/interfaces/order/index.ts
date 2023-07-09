79dd6de25f3a93cdd9105d55e4b6d23ae743904d
roq-ai <>
2023-07-09 16:17:31 +0000

    Initial commit

---
+++ src/interfaces/order/index.ts
@@ -0,0 +1,25 @@
+import { UserInterface } from 'interfaces/user';
+import { OrganizationInterface } from 'interfaces/organization';
+import { GetQueryInterface } from 'interfaces';
+
+export interface OrderInterface {
+  id?: string;
+  status: string;
+  details: string;
+  user_id?: string;
+  organization_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  user?: UserInterface;
+  organization?: OrganizationInterface;
+  _count?: {};
+}
+
+export interface OrderGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  status?: string;
+  details?: string;
+  user_id?: string;
+  organization_id?: string;
+}
---
+++ src/validationSchema/orders/index.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const orderValidationSchema = yup.object().shape({
+  status: yup.string().required(),
+  details: yup.string().required(),
+  user_id: yup.string().nullable(),
+  organization_id: yup.string().nullable(),
+});
